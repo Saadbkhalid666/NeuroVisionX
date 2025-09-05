284d7f26c79bcb93c40293a413d1597cba3a87a4
Saad Bin Khalid <khaildmehmoodeditz@gmail.com>
2025-09-05 17:21:21 +0500

    Re Initializing project n app

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,26 +1,3 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import UploadedImage
+from django.shortcuts import render
 
-@csrf_exempt
-def upload_image(request):
-    if request.method == 'POST':
-        if 'file' in request.FILES:
-            file = request.FILES['file']
-            data = file.read()  # read file bytes
-
-            # Save to DB
-            uploaded_image = UploadedImage.objects.create(
-                name=file.name,
-                data=data,
-                content_type=file.content_type
-            )
-
-            return JsonResponse({
-                "message": "File uploaded successfully!",
-                "filename": uploaded_image.name,
-            })
-
-        return JsonResponse({"error": "No file uploaded"}, status=400)
-
-    return JsonResponse({"error": "Only POST allowed"}, status=405)
+# Create your views here.
